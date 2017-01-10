3abdc91d49d4413ed96bab8c1c4e62f610bd370c
Franklin Harvey <Franklinadamsharvey@gmail.com>
2017-01-09 20:41:40 -0700

    fix table alignment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayList } from './components/SkiDayList'
 
 window.React = React
 
-
 render(
-	<SkiDayCount total={50}
-				 powder={20}
-				 backcountry={10}
-				 goal={100}/>,
+	<SkiDayList days={
+		[
+			{
+				resort: "Squaw",
+				date: new Date("1/2/2016"),
+				powder: true,
+				backcountry: false
+			},
+			{
+				resort: "Kirk",
+				date: new Date("3/28/2016"),
+				powder: false,
+				backcountry: false
+			},
+			{
+				resort: "Mammoth",
+				date: new Date("4/2/2016"),
+				powder: false,
+				backcountry: true
+			}
+		]
+	}/>,
 	document.getElementById('react-container')
 )
