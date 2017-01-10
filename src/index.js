97cf4bea682ab0b7811b6cc6b1f068c19eaf3271
Franklin Harvey <Franklinadamsharvey@gmail.com>
2017-01-10 10:54:11 -0700

    broken server

--- src/index.js
+++ src/index.js
@@ -8,19 +8,19 @@ render(
 	<SkiDayList days={
 		[
 			{
-				resort: "Squaw",
+				resort: "Squaw Valley",
 				date: new Date("1/2/2016"),
 				powder: true,
 				backcountry: false
 			},
 			{
-				resort: "Kirk",
+				resort: "Kirkwood",
 				date: new Date("3/28/2016"),
 				powder: false,
 				backcountry: false
 			},
 			{
-				resort: "Mammoth",
+				resort: "Mt. Tallac",
 				date: new Date("4/2/2016"),
 				powder: false,
 				backcountry: true
