87341862b35fd148b2826a74b584898139778e11
Franklin Harvey <Franklinadamsharvey@gmail.com>
2017-01-11 11:40:35 -0700

    prop types

--- src/components/SkiDayList.js
+++ src/components/SkiDayList.js
@@ -5,18 +5,19 @@ import { SkiDayRow } from './SkiDayRow'
 
 export const SkiDayList = ({days}) => (
 	<table>
-		<tr>
-			<th>Date</th>
-			<th>Resort</th>
-			<th>Powder</th>
-			<th>Backcountry</th>
-		</tr>
+		<thead>
+			<tr>
+				<th>Date</th>
+				<th>Resort</th>
+				<th>Powder</th>
+				<th>Backcountry</th>
+			</tr>
+		</thead>
 		<tbody>
 			{days.map((day, i) =>
 				<SkiDayRow key={i}
 						   {...day}/>
 				)}
 		</tbody>
-
 	</table>
 )
--- src/index.js
+++ src/index.js
@@ -1,31 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayList } from './components/SkiDayList'
+import { SkiDayCount } from './components/SkiDayCount-createClass'
 
 window.React = React
 
 render(
-	<SkiDayList days={
-		[
-			{
-				resort: "Squaw",
-				date: new Date("1/2/2016"),
-				powder: true,
-				backcountry: false
-			},
-			{
-				resort: "Kirk",
-				date: new Date("3/28/2016"),
-				powder: false,
-				backcountry: false
-			},
-			{
-				resort: "Mammoth",
-				date: new Date("4/2/2016"),
-				powder: false,
-				backcountry: true
-			}
-		]
-	}/>,
+	<SkiDayCount total="lots"/>,
 	document.getElementById('react-container')
 )
